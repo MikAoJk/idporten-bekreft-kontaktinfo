7c54cf221a64438f2592e54edb539705003ec1e0
Jan Fretheim <jan.fretheim@digdir.no>
2020-12-07 09:05:17 +0100

    login virker

--- idporten-bekreft-kontaktinfo-web/src/main/java/no/digdir/krr/bekreft/kontaktinfo/crypto/JwtSigningService.java
+++ idporten-bekreft-kontaktinfo-web/src/main/java/no/digdir/krr/bekreft/kontaktinfo/crypto/JwtSigningService.java
@@ -1,27 +1,27 @@
 package no.digdir.krr.bekreft.kontaktinfo.crypto;
 
 import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JOSEObjectType;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class JwtSigningService {
 
     private final KeyProvider keyProvider;
-
-    public JwtSigningService(KeyProvider keyProvider) {
-        this.keyProvider = keyProvider;
-    }
 
     public String signJwt(JWTClaimsSet claims) {
         try {
             JWSSigner signer = new RSASSASigner(keyProvider.privateKey());
             JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256)
+                    .type(JOSEObjectType.JWT)
                     .build();
             SignedJWT signedJWT = new SignedJWT(header, claims);
             signedJWT.sign(signer);
